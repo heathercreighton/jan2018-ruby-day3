e0b27e37b05c9ebf8c408f3eedaf90bd4f3adfc6
heathercreighton <heather.creighton@gmail.com>
2018-01-04 10:25:40 -0500

    Added another break between programs

--- method_practice.rb
+++ method_practice.rb
@@ -63,7 +63,7 @@
 
 # puts "Our new hash is #{array_to_hash(fruit)}"
 
-
+#******************************************************
 # Write a program that asks for two (2) Integers, divides the first by the second and returns the result including the remainder. If either of the numbers is not an Integer, then don't accept the number and ask again.
 
 #  Do not accept zero (0) as a number.
